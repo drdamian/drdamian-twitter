4aa3c300df9ecc87448df82f39d34fa747da4d1f
Damian Bariexca <damian@bariexca.net>
2017-02-23 22:19:16 -0500

    Update user_details.js

--- data/js/user_details.js
+++ data/js/user_details.js
@@ -4,7 +4,7 @@ var user_details =  {
   "location" : "Perkasie, PA USA",
   "url" : "http:\/\/t.co\/0mfonM3zAp",
   "full_name" : "Dr. Damian Bariexca",
-  "bio" : "#LTPS Supervisor of EdTech & Arts. #NJED since 2000. Standard disclaimer: Tweets \u2260 opinion of my employer.",
+  "bio" : "#LTPS Supervisor of EdTech & Arts. #NJED since 2000. Tweets \u2260 opinion of my employer.",
   "id" : "7887852",
   "created_at" : "2007-08-01 18:19:19 +0000",
   "display_url" : "drdamian.org"
