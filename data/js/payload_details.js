6e2706f9dd76c317ae65ca549ed0f1b09e3b9e4c
Damian Bariexca <damian@bariexca.net>
2019-02-05 20:16:49 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31749,
-	"created_at": "Wed Feb 06 01:11:22 +0000 2019",
+	"tweets": 31750,
+	"created_at": "Wed Feb 06 01:16:48 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -175,6 +175,180 @@ Grailbird.data.tweets_2019_02 =
 		"text": "Spirit Week is next week! Seniors it’s our last spirit week!! https://t.co/5ivG3HF5Y4"
 	},
 	{
+		"created_at": "Wed Feb 06 00:05:26 +0000 2019",
+		"id": 1092937273946054700,
+		"id_str": "1092937273946054656",
+		"full_text": "Spirit Week is next week! Seniors it’s our last spirit week!! https://t.co/5ivG3HF5Y4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1092937269714006000,
+					"id_str": "1092937269714006016",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/Dyrkp_sWsAA7ZHN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dyrkp_sWsAA7ZHN.jpg",
+					"url": "https://t.co/5ivG3HF5Y4",
+					"display_url": "pic.twitter.com/5ivG3HF5Y4",
+					"expanded_url": "https://twitter.com/LAWRENCE_2019/status/1092937273946054656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1118,
+							"h": 753,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1118,
+							"h": 753,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 458,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1092937269714006000,
+					"id_str": "1092937269714006016",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/Dyrkp_sWsAA7ZHN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dyrkp_sWsAA7ZHN.jpg",
+					"url": "https://t.co/5ivG3HF5Y4",
+					"display_url": "pic.twitter.com/5ivG3HF5Y4",
+					"expanded_url": "https://twitter.com/LAWRENCE_2019/status/1092937273946054656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1118,
+							"h": 753,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1118,
+							"h": 753,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 458,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4900074347,
+			"id_str": "4900074347",
+			"name": "LHS '19",
+			"screen_name": "LAWRENCE_2019",
+			"location": "Lawrence, NJ",
+			"description": "Official Twitter for Lawrence High School's Class of 2019 🔴⚪️",
+			"url": "https://t.co/JtNUPagyAE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/JtNUPagyAE",
+							"expanded_url": "http://ltps.org",
+							"display_url": "ltps.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 107,
+			"friends_count": 119,
+			"listed_count": 0,
+			"created_at": "Fri Feb 12 02:32:13 +0000 2016",
+			"favourites_count": 58,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 164,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/774730972386451456/xYjwpzLK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/774730972386451456/xYjwpzLK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4900074347/1538617246",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Spirit Week is next week! Seniors it’s our last spirit week!! https://t.co/5ivG3HF5Y4"
+	},
+	{
 		"created_at": "Tue Feb 05 16:51:42 +0000 2019",
 		"id": 1092828119591993300,
 		"id_str": "1092828119591993345",
