424298112bcb57d445ab9a610d9a0c0f6cf7242e
Damian Bariexca <damian@bariexca.net>
2019-04-30 21:11:24 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31982,
-	"created_at": "Tue Apr 30 01:11:22 +0000 2019",
+	"tweets": 31983,
+	"created_at": "Wed May 01 01:11:22 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Tue Apr 30 17:09:44 +0000 2019",
+		"id": 1123256165771743200,
+		"id_str": "1123273239948681216",
+		"full_text": "Congratulations to all of our LHS DECA members earning the opportunity to compete at ICDC! We have 6 members competing against members across the world as finalist. Grand Awards this evening! We are very proud of all of our members! @SchneckDiane @MsPalombo @dadamltps #LTPS #DECA https://t.co/RfIUt5XgEI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LTPS",
+					"indices": [
+						269,
+						274
+					]
+				},
+				{
+					"text": "DECA",
+					"indices": [
+						275,
+						280
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SchneckDiane",
+					"name": "Diane Schneck",
+					"id": 4901883603,
+					"id_str": "4901883603",
+					"indices": [
+						233,
+						246
+					]
+				},
+				{
+					"screen_name": "MsPalombo",
+					"name": "Ms. Palombo",
+					"id": 858779476783538200,
+					"id_str": "858779476783538176",
+					"indices": [
+						247,
+						257
+					]
+				},
+				{
+					"screen_name": "dadamltps",
+					"name": "David Adam",
+					"id": 260753316,
+					"id_str": "260753316",
+					"indices": [
+						258,
+						268
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1123256160000385000,
+					"id_str": "1123256160000385026",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D5abhriXoAIvxTn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5abhriXoAIvxTn.jpg",
+					"url": "https://t.co/RfIUt5XgEI",
+					"display_url": "pic.twitter.com/RfIUt5XgEI",
+					"expanded_url": "https://twitter.com/john_tees/status/1123256165771743233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 508,
+							"h": 678,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 508,
+							"h": 678,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 508,
+							"h": 678,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123256160000385000,
+					"id_str": "1123256160000385026",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D5abhriXoAIvxTn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5abhriXoAIvxTn.jpg",
+					"url": "https://t.co/RfIUt5XgEI",
+					"display_url": "pic.twitter.com/RfIUt5XgEI",
+					"expanded_url": "https://twitter.com/john_tees/status/1123256165771743233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 508,
+							"h": 678,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 508,
+							"h": 678,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 508,
+							"h": 678,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1123256159979417600,
+					"id_str": "1123256159979417601",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D5abhrdXsAEJzUS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5abhrdXsAEJzUS.jpg",
+					"url": "https://t.co/RfIUt5XgEI",
+					"display_url": "pic.twitter.com/RfIUt5XgEI",
+					"expanded_url": "https://twitter.com/john_tees/status/1123256165771743233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 801118345,
+			"id_str": "801118345",
+			"name": "John Tees",
+			"screen_name": "john_tees",
+			"location": "",
+			"description": "High School Business teacher and Co-DECA Advisor In Lawrenceville, NJ. Solely my comments.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 76,
+			"friends_count": 99,
+			"listed_count": 1,
+			"created_at": "Mon Sep 03 20:03:32 +0000 2012",
+			"favourites_count": 115,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 60,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1103658353404850176/Asky8PI__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1103658353404850176/Asky8PI__normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 28,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Congratulations to all of our LHS DECA members earning the opportunity to compete at ICDC! We have 6 members competing against members across the world as finalist. Grand Awards this evening! We are very proud of all of our members! @SchneckDiane @MsPalombo @dadamltps #LTPS #DECA https://t.co/RfIUt5XgEI"
+	},
+	{
 		"created_at": "Mon Apr 29 23:07:05 +0000 2019",
 		"id": 1122993445474381800,
 		"id_str": "1123000781395963906",
