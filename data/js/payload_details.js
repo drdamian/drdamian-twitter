58a5d3604ee97a8f57a81b35c53c24e76328f1f7
Damian Bariexca <damian@bariexca.net>
2019-08-03 21:11:22 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32117,
-	"created_at": "Fri Aug 02 01:11:22 +0000 2019",
+	"tweets": 32118,
+	"created_at": "Sun Aug 04 01:11:21 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 03 14:18:18 +0000 2019",
+		"id": 1157656945349845000,
+		"id_str": "1157656945349844992",
+		"full_text": "Same.  Still suggest but with strong caveats. https://t.co/nkpeecBsBT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			45
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/nkpeecBsBT",
+					"expanded_url": "https://twitter.com/chrisjcluff/status/1157648997449904132",
+					"display_url": "twitter.com/chrisjcluff/st…",
+					"indices": [
+						46,
+						69
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA USA",
+			"description": "Assistant Principal, Lawrence High School ~ Adjunct, @DelVal ~ Dad ~ Husband ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets ≠ opinion of my employers.",
+			"url": "https://t.co/0mfonM3zAp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0mfonM3zAp",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3223,
+			"friends_count": 1526,
+			"listed_count": 261,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2004,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 32107,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1111809974143987713/-kZ7O0zJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1111809974143987713/-kZ7O0zJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1157648997449904000,
+		"quoted_status_id_str": "1157648997449904132",
+		"quoted_status_permalink": {
+			"url": "https://t.co/nkpeecBsBT",
+			"expanded": "https://twitter.com/chrisjcluff/status/1157648997449904132",
+			"display": "twitter.com/chrisjcluff/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Aug 03 13:46:43 +0000 2019",
+			"id": 1157648997449904000,
+			"id_str": "1157648997449904132",
+			"full_text": "Has the shape of HOW&amp;WHY you'd promote Twitter 2 EDUs changd? When I startd playing w Twitter I'd tell any1 listening 2 just start using it. &amp; any1 that didn't get my pitch, I'd say 'just try it&amp;you'll get it.' I wouldn't say that now. &amp;I'm also not asquick 2promote the platform.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				296
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1002328287941546000,
+				"id_str": "1002328287941545985",
+				"name": "คppētitē f໐r iຖŞtrน¢ti໐ຖ",
+				"screen_name": "chrisjcluff",
+				"location": "",
+				"description": "Reach out, my DMs are open. his/him/he=me #decodedpodcast #chasingsquirrelspodcast #commuterclassroom",
+				"url": "https://t.co/IjQqRoJRn6",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/IjQqRoJRn6",
+								"expanded_url": "https://chrisjcluff.com/aboutme/",
+								"display_url": "chrisjcluff.com/aboutme/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 549,
+				"friends_count": 1232,
+				"listed_count": 4,
+				"created_at": "Thu May 31 23:17:40 +0000 2018",
+				"favourites_count": 255,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 2708,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1154374292173402117/BuQ35ld2_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1154374292173402117/BuQ35ld2_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1002328287941545985/1564059254",
+				"profile_link_color": "FF691F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Same.  Still suggest but with strong caveats. https://t.co/nkpeecBsBT"
+	},
+	{
 		"created_at": "Thu Aug 01 17:00:49 +0000 2019",
 		"id": 1156973065974272000,
 		"id_str": "1156973065974272001",
