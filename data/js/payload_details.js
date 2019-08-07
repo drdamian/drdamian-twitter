14d86ac2e2797b67da9014bfa69d934c1ec45740
Damian Bariexca <damian@bariexca.net>
2019-08-06 21:11:23 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32120,
-	"created_at": "Tue Aug 06 01:11:21 +0000 2019",
+	"tweets": 32121,
+	"created_at": "Wed Aug 07 01:11:22 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Tue Aug 06 13:41:02 +0000 2019",
+		"id": 1158731893640224800,
+		"id_str": "1158734730466156546",
+		"full_text": "This Toni Morrison quote changed my life. https://t.co/oLHbqolz25",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1158731890993614800,
+					"id_str": "1158731890993614848",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/EBSkhACXUAAI-bH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBSkhACXUAAI-bH.jpg",
+					"url": "https://t.co/oLHbqolz25",
+					"display_url": "pic.twitter.com/oLHbqolz25",
+					"expanded_url": "https://twitter.com/aishacs/status/1158731893640224768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 765,
+							"h": 1182,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 765,
+							"h": 1182,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 440,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1158731890993614800,
+					"id_str": "1158731890993614848",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/EBSkhACXUAAI-bH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBSkhACXUAAI-bH.jpg",
+					"url": "https://t.co/oLHbqolz25",
+					"display_url": "pic.twitter.com/oLHbqolz25",
+					"expanded_url": "https://twitter.com/aishacs/status/1158731893640224768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 765,
+							"h": 1182,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 765,
+							"h": 1182,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 440,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15809934,
+			"id_str": "15809934",
+			"name": "Aisha Saeed",
+			"screen_name": "aishacs",
+			"location": "Atlanta",
+			"description": "NYT bestselling author of AMAL UNBOUND. Out now: BILAL COOKS DAAL. Up next: YES NO MAYBE SO 2/2020. Drinker of chai. Founding member of #WeNeedDiverseBooks",
+			"url": "https://t.co/sHW029SYi7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/sHW029SYi7",
+							"expanded_url": "http://www.aishasaeed.com",
+							"display_url": "aishasaeed.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20365,
+			"friends_count": 1202,
+			"listed_count": 509,
+			"created_at": "Mon Aug 11 16:20:52 +0000 2008",
+			"favourites_count": 46215,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4426,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/803242947181506561/Np_u2zGv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/803242947181506561/Np_u2zGv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15809934/1509202328",
+			"profile_link_color": "990000",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9184,
+		"favorite_count": 31978,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This Toni Morrison quote changed my life. https://t.co/oLHbqolz25"
+	},
+	{
 		"created_at": "Mon Aug 05 23:23:11 +0000 2019",
 		"id": 1158518846304784400,
 		"id_str": "1158518846304784389",
