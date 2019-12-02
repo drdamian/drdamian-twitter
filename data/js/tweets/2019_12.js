a6362095584dde98bd0f5e69e5b3b6a7ab210cd1
Damian Bariexca <damian@bariexca.net>
2019-12-01 20:11:23 -0500

    Added 1 tweets

---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,141 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Sun Dec 01 20:05:43 +0000 2019",
+		"id": 1201230918054228000,
+		"id_str": "1201230918054227973",
+		"full_text": "@jpalladino @SGSFOX @stateofamorica @theblackcrowes I'm about halfway through the audiobook version; can't put it down!",
+		"truncated": false,
+		"display_text_range": [
+			52,
+			119
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jpalladino",
+					"name": "jpalladino",
+					"id": 14538491,
+					"id_str": "14538491",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "SGSFOX",
+					"name": "Steve Gorman",
+					"id": 105641617,
+					"id_str": "105641617",
+					"indices": [
+						12,
+						19
+					]
+				},
+				{
+					"screen_name": "stateofamorica",
+					"name": "State of Amorica Podcast",
+					"id": 1063954830530629600,
+					"id_str": "1063954830530629632",
+					"indices": [
+						20,
+						35
+					]
+				},
+				{
+					"screen_name": "theblackcrowes",
+					"name": "TheBlackCrowes",
+					"id": 1066616059,
+					"id_str": "1066616059",
+					"indices": [
+						36,
+						51
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1201225324102660000,
+		"in_reply_to_status_id_str": "1201225324102660099",
+		"in_reply_to_user_id": 14538491,
+		"in_reply_to_user_id_str": "14538491",
+		"in_reply_to_screen_name": "jpalladino",
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA USA",
+			"description": "Asst. Principal, @CardinalsLHS ~ Adjunct, @DelVal ~ Dad ~ Husband ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/0mfonM3zAp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0mfonM3zAp",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3239,
+			"friends_count": 1548,
+			"listed_count": 258,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2162,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 32406,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1183538790348021761/5oScTqFU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1183538790348021761/5oScTqFU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@jpalladino @SGSFOX @stateofamorica @theblackcrowes I'm about halfway through the audiobook version; can't put it down!"
+	}
+]
