2b99e351661bdaa730b788851d9a7d03d43b6433
Damian Bariexca <damian@bariexca.net>
2020-02-03 20:11:23 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32445,
-	"created_at": "Sun Feb 02 01:11:22 +0000 2020",
+	"tweets": 32446,
+	"created_at": "Tue Feb 04 01:11:23 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,114 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 03 22:30:56 +0000 2020",
+		"id": 1224460285651619800,
+		"id_str": "1224460285651619840",
+		"full_text": "I'm at Delaware Valley University - @delvalcollege in Doylestown, PA https://t.co/BLl16UqJ20",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			92
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/BLl16UqJ20",
+					"expanded_url": "https://www.swarmapp.com/c/9YFbJIow6Uw",
+					"display_url": "swarmapp.com/c/9YFbJIow6Uw",
+					"indices": [
+						69,
+						92
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://foursquare.com\" rel=\"nofollow\">Foursquare</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA USA",
+			"description": "Asst. Principal, @CardinalsLHS ~ Adjunct, @DelVal ~ Dad ~ Husband ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets & blogs ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/0mfonM3zAp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0mfonM3zAp",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3233,
+			"friends_count": 1549,
+			"listed_count": 257,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2210,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 32535,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1183538790348021761/5oScTqFU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1183538790348021761/5oScTqFU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I'm at Delaware Valley University - @delvalcollege in Doylestown, PA https://t.co/BLl16UqJ20"
+	},
+	{
 		"created_at": "Sat Feb 01 21:20:29 +0000 2020",
 		"id": 1223717781222367200,
 		"id_str": "1223717781222367233",
@@ -230,7 +338,7 @@ Grailbird.data.tweets_2020_02 =
 		"created_at": "Sat Feb 01 21:15:39 +0000 2020",
 		"id": 1223716564127703000,
 		"id_str": "1223716564127703044",
-		"full_text": "@JenAnsbach 🤷🏼‍♂️ After owing over $2700 to the feds last year, I'll take it.  The real gut-punch was accidentally coming across a pre-Chapter 78 paystub and seeing how little my net pay has increased since then, despite me grossing significantly more than I used to.",
+		"full_text": "@JenAnsbach 🤷🏼♂️ After owing over $2700 to the feds last year, I'll take it.  The real gut-punch was accidentally coming across a pre-Chapter 78 paystub and seeing how little my net pay has increased since then, despite me grossing significantly more than I used to.",
 		"truncated": false,
 		"display_text_range": [
 			12,
@@ -332,6 +440,6 @@ Grailbird.data.tweets_2020_02 =
 		"favorited": false,
 		"retweeted": false,
 		"lang": "en",
-		"text": "@JenAnsbach 🤷🏼‍♂️ After owing over $2700 to the feds last year, I'll take it.  The real gut-punch was accidentally coming across a pre-Chapter 78 paystub and seeing how little my net pay has increased since then, despite me grossing significantly more than I used to."
+		"text": "@JenAnsbach 🤷🏼♂️ After owing over $2700 to the feds last year, I'll take it.  The real gut-punch was accidentally coming across a pre-Chapter 78 paystub and seeing how little my net pay has increased since then, despite me grossing significantly more than I used to."
 	}
 ]
