91ebf59323913634d86f5df4b9c1c5336d23a061
Damian Bariexca <damian@bariexca.net>
2020-03-24 21:11:24 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32552,
-	"created_at": "Tue Mar 24 01:11:22 +0000 2020",
+	"tweets": 32553,
+	"created_at": "Wed Mar 25 01:11:22 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Tue Mar 24 02:10:58 +0000 2020",
+		"id": 1242271303782056000,
+		"id_str": "1242272663818711048",
+		"full_text": "A Trauma-Informed Approach to Teaching Through Coronavirus https://t.co/2RfvloBHVo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			82
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2RfvloBHVo",
+					"expanded_url": "https://www.tolerance.org/magazine/a-trauma-informed-approach-to-teaching-through-coronavirus",
+					"display_url": "tolerance.org/magazine/a-tra…",
+					"indices": [
+						59,
+						82
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 248447187,
+			"id_str": "248447187",
+			"name": "Christy Brennan",
+			"screen_name": "christybrenn",
+			"location": "Pennsylvania",
+			"description": "MS Teacher/Librarian passionately curious about creativity, empathy, design-thinking, equity & innovation. @penngse @rosemontcollege alumna #GoogleET #dtk12chat",
+			"url": "https://t.co/fLkY9BC4Xw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fLkY9BC4Xw",
+							"expanded_url": "http://christy-brennan.com",
+							"display_url": "christy-brennan.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2722,
+			"friends_count": 2866,
+			"listed_count": 241,
+			"created_at": "Mon Feb 07 01:08:25 +0000 2011",
+			"favourites_count": 10088,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10968,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/790341203740987392/EOdtZJiH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/790341203740987392/EOdtZJiH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/248447187/1436309827",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A Trauma-Informed Approach to Teaching Through Coronavirus https://t.co/2RfvloBHVo"
+	},
+	{
 		"created_at": "Tue Mar 24 01:05:09 +0000 2020",
 		"id": 1242256101820772400,
 		"id_str": "1242256101820772354",
