d4dce56cf11629e629b36a91d4325a3ec5135fff
Damian Bariexca <damian@bariexca.net>
2020-04-02 21:11:23 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32580,
-	"created_at": "Wed Apr 01 01:11:23 +0000 2020",
+	"tweets": 32581,
+	"created_at": "Fri Apr 03 01:11:22 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,321 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Thu Apr 02 02:49:19 +0000 2020",
+		"id": 1245543807757750300,
+		"id_str": "1245543807757750273",
+		"full_text": "One of my favorite bands for over 20 years and an unbelievably talented musician and songwriter.  FoW was one of those bands I really felt was 'my' band, in a weird way.  I don't often weigh in on celebrities passing but this one hits a bit different for me. https://t.co/kcGsAE6Shs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			258
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kcGsAE6Shs",
+					"expanded_url": "https://twitter.com/RollingStone/status/1245478464389603328",
+					"display_url": "twitter.com/RollingStone/s…",
+					"indices": [
+						259,
+						282
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca is (literally) in the house 🏠",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA USA",
+			"description": "Asst. Principal, @CardinalsLHS ~ Adjunct, @DelVal ~ Dad ~ Husband ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets & blogs ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/0mfonM3zAp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0mfonM3zAp",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3229,
+			"friends_count": 1549,
+			"listed_count": 255,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2279,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 32710,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1183538790348021761/5oScTqFU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1183538790348021761/5oScTqFU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1245478464389603300,
+		"quoted_status_id_str": "1245478464389603328",
+		"quoted_status_permalink": {
+			"url": "https://t.co/kcGsAE6Shs",
+			"expanded": "https://twitter.com/RollingStone/status/1245478464389603328",
+			"display": "twitter.com/RollingStone/s…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Apr 01 22:29:40 +0000 2020",
+			"id": 1245478464389603300,
+			"id_str": "1245478464389603328",
+			"full_text": "Adam Schlesinger, co-founder of Fountains of Wayne and Emmy-winning composer for 'Crazy Ex-Girlfriend,' has died at 52 from complications related to the coronavirus https://t.co/acTqEhmUqV https://t.co/OULDOX3cod",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				188
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/acTqEhmUqV",
+						"expanded_url": "https://rol.st/341JVAw",
+						"display_url": "rol.st/341JVAw",
+						"indices": [
+							165,
+							188
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1245478460283379700,
+						"id_str": "1245478460283379718",
+						"indices": [
+							189,
+							212
+						],
+						"media_url": "http://pbs.twimg.com/media/EUjUD5CXYAY_Oi5.png",
+						"media_url_https": "https://pbs.twimg.com/media/EUjUD5CXYAY_Oi5.png",
+						"url": "https://t.co/OULDOX3cod",
+						"display_url": "pic.twitter.com/OULDOX3cod",
+						"expanded_url": "https://twitter.com/RollingStone/status/1245478464389603328/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 452,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 747,
+								"h": 497,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 747,
+								"h": 497,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1245478460283379700,
+						"id_str": "1245478460283379718",
+						"indices": [
+							189,
+							212
+						],
+						"media_url": "http://pbs.twimg.com/media/EUjUD5CXYAY_Oi5.png",
+						"media_url_https": "https://pbs.twimg.com/media/EUjUD5CXYAY_Oi5.png",
+						"url": "https://t.co/OULDOX3cod",
+						"display_url": "pic.twitter.com/OULDOX3cod",
+						"expanded_url": "https://twitter.com/RollingStone/status/1245478464389603328/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 452,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 747,
+								"h": 497,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 747,
+								"h": 497,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://www.socialflow.com\" rel=\"nofollow\">SocialFlow</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14780915,
+				"id_str": "14780915",
+				"name": "Rolling Stone",
+				"screen_name": "RollingStone",
+				"location": "New York, New York",
+				"description": "The latest news and more from Rolling Stone magazine and https://t.co/nFMXaq9sbV.  Got a tip? Share it here: https://t.co/JkxAZoupLY",
+				"url": "http://t.co/zhpWt9kvuA",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/zhpWt9kvuA",
+								"expanded_url": "http://www.rollingstone.com",
+								"display_url": "rollingstone.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/nFMXaq9sbV",
+								"expanded_url": "http://RollingStone.com",
+								"display_url": "RollingStone.com",
+								"indices": [
+									57,
+									80
+								]
+							},
+							{
+								"url": "https://t.co/JkxAZoupLY",
+								"expanded_url": "http://rol.st/tips",
+								"display_url": "rol.st/tips",
+								"indices": [
+									109,
+									132
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 6313429,
+				"friends_count": 336,
+				"listed_count": 38782,
+				"created_at": "Thu May 15 02:52:27 +0000 2008",
+				"favourites_count": 131,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 126557,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "0B0B0E",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1225871981196660739/m7oam68-_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225871981196660739/m7oam68-_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14780915/1585256323",
+				"profile_link_color": "2FC2EF",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4379,
+			"favorite_count": 8548,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "One of my favorite bands for over 20 years and an unbelievably talented musician and songwriter.  FoW was one of those bands I really felt was 'my' band, in a weird way.  I don't often weigh in on celebrities passing but this one hits a bit different for me. https://t.co/kcGsAE6Shs"
+	},
+	{
 		"created_at": "Wed Apr 01 00:03:26 +0000 2020",
 		"id": 1245139673095254000,
 		"id_str": "1245139673095254016",
