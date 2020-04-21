880a897b78134fba5ea0d5dcf666741a200c63a9
Damian Bariexca <damian@bariexca.net>
2020-04-20 21:11:23 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32609,
-	"created_at": "Mon Apr 20 01:11:22 +0000 2020",
+	"tweets": 32610,
+	"created_at": "Tue Apr 21 01:11:22 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Mon Apr 20 23:05:17 +0000 2020",
+		"id": 1252372797805887500,
+		"id_str": "1252372797805887495",
+		"full_text": "Absolutely tuning in for my last chance to see one of my favorite bands. https://t.co/3OIxIgXGIw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			72
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3OIxIgXGIw",
+					"expanded_url": "https://twitter.com/fountainsofwayn/status/1252364241765494785",
+					"display_url": "twitter.com/fountainsofway…",
+					"indices": [
+						73,
+						96
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca is (literally) in the house 🏠",
+			"screen_name": "_drdamian",
+			"location": "More PA than NJ these days ",
+			"description": "Asst. Principal, @CardinalsLHS ~ Adjunct, @DelVal ~ Dad & husband ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets & blogs ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/0mfonM3zAp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0mfonM3zAp",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3225,
+			"friends_count": 1542,
+			"listed_count": 253,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2297,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 32750,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1247260112194068480/gU5RRhS8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1247260112194068480/gU5RRhS8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1252364241765494800,
+		"quoted_status_id_str": "1252364241765494785",
+		"quoted_status_permalink": {
+			"url": "https://t.co/3OIxIgXGIw",
+			"expanded": "https://twitter.com/fountainsofwayn/status/1252364241765494785",
+			"display": "twitter.com/fountainsofway…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Apr 20 22:31:17 +0000 2020",
+			"id": 1252364241765494800,
+			"id_str": "1252364241765494785",
+			"full_text": "In honor of Adam, we will be performing with @sharonvanetten as part of @nj_prf's JERSEY 4 JERSEY, a benefit show raising funds to fight the impact of COVID-19. The concert airs this Wednesday at 7PM ET, more info: https://t.co/MWvyWYjZPr",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				238
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "sharonvanetten",
+						"name": "Sharon Van Etten",
+						"id": 22184631,
+						"id_str": "22184631",
+						"indices": [
+							45,
+							60
+						]
+					},
+					{
+						"screen_name": "NJ_PRF",
+						"name": "New Jersey Pandemic Relief Fund",
+						"id": 1240988555863867400,
+						"id_str": "1240988555863867397",
+						"indices": [
+							72,
+							79
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/MWvyWYjZPr",
+						"expanded_url": "https://njprf.org/",
+						"display_url": "njprf.org",
+						"indices": [
+							215,
+							238
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 101924614,
+				"id_str": "101924614",
+				"name": "Fountains of Wayne",
+				"screen_name": "fountainsofwayn",
+				"location": "New York, NY",
+				"description": "",
+				"url": "http://t.co/jns2XLXreu",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/jns2XLXreu",
+								"expanded_url": "http://www.fountainsofwayne.com",
+								"display_url": "fountainsofwayne.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 9080,
+				"friends_count": 111,
+				"listed_count": 212,
+				"created_at": "Tue Jan 05 01:57:14 +0000 2010",
+				"favourites_count": 8,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 626,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1393975609/0679898263_m_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1393975609/0679898263_m_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/101924614/1348182324",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 46,
+			"favorite_count": 188,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Absolutely tuning in for my last chance to see one of my favorite bands. https://t.co/3OIxIgXGIw"
+	},
+	{
 		"created_at": "Sun Apr 19 18:50:09 +0000 2020",
 		"id": 1251946201605439500,
 		"id_str": "1251946201605439494",
