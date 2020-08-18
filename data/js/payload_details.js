27dee60636bfba95fe38b98074e8a6b48da160e7
Damian Bariexca <damian@bariexca.net>
2020-08-17 21:11:24 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32714,
-	"created_at": "Mon Aug 17 01:11:22 +0000 2020",
+	"tweets": 32715,
+	"created_at": "Tue Aug 18 01:11:22 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Mon Aug 17 23:09:50 +0000 2020",
+		"id": 1295498097049702400,
+		"id_str": "1295498097049702400",
+		"full_text": "@SukhiVG @Yulieth_Le https://t.co/SMnvEEvnDW",
+		"truncated": false,
+		"display_text_range": [
+			20,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SukhiVG",
+					"name": "Sukhi VG",
+					"id": 1712753586,
+					"id_str": "1712753586",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "Yulieth_Le",
+					"name": "Mrs. Le’s Fourth Grade Leaders",
+					"id": 2310286079,
+					"id_str": "2310286079",
+					"indices": [
+						9,
+						20
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1295498040573333500,
+					"id_str": "1295498040573333506",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/EfqImqOWkAIRNfN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfqImqOWkAIRNfN.jpg",
+					"url": "https://t.co/SMnvEEvnDW",
+					"display_url": "pic.twitter.com/SMnvEEvnDW",
+					"expanded_url": "https://twitter.com/_drdamian/status/1295498097049702400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1295498040573333500,
+					"id_str": "1295498040573333506",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/EfqImqOWkAIRNfN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfqImqOWkAIRNfN.jpg",
+					"url": "https://t.co/SMnvEEvnDW",
+					"display_url": "pic.twitter.com/SMnvEEvnDW",
+					"expanded_url": "https://twitter.com/_drdamian/status/1295498097049702400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 270,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1295455968633983000,
+		"in_reply_to_status_id_str": "1295455968633982986",
+		"in_reply_to_user_id": 1712753586,
+		"in_reply_to_user_id_str": "1712753586",
+		"in_reply_to_screen_name": "SukhiVG",
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca is still wearing his mask",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal, @CardinalsLHS ~ Dad & husband ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets & blogs ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/0mfonM3zAp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0mfonM3zAp",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3207,
+			"friends_count": 1561,
+			"listed_count": 247,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2352,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 32885,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1247260112194068480/gU5RRhS8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1247260112194068480/gU5RRhS8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@SukhiVG @Yulieth_Le https://t.co/SMnvEEvnDW"
+	},
+	{
 		"created_at": "Sun Aug 16 01:28:03 +0000 2020",
 		"id": 1294808107810513000,
 		"id_str": "1294808107810512896",
