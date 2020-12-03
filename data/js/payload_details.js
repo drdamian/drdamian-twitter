b004f7669b9843e96628b497c8d3c2e4f7beb1d9
Damian Bariexca <damian@bariexca.net>
2020-12-02 20:11:23 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32796,
-	"created_at": "Sun Nov 29 01:11:21 +0000 2020",
+	"tweets": 32797,
+	"created_at": "Thu Dec 03 01:11:22 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_12.js
@@ -0,0 +1,210 @@
+Grailbird.data.tweets_2020_12 = 
+[
+	{
+		"created_at": "Wed Dec 02 16:35:00 +0000 2020",
+		"id": 1334174236517261300,
+		"id_str": "1334174236517261312",
+		"full_text": "Thread 👇 https://t.co/vcVmJDwHCz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			8
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vcVmJDwHCz",
+					"expanded_url": "https://twitter.com/kasal_finley/status/1333966411467788288",
+					"display_url": "twitter.com/kasal_finley/s…",
+					"indices": [
+						9,
+						32
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca is still wearing his mask 😷",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal, @CardinalsLHS ~ Dad & husband ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets & blogs ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/0mfonM3zAp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0mfonM3zAp",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3195,
+			"friends_count": 1559,
+			"listed_count": 247,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2391,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 32992,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1333966411467788300,
+		"quoted_status_id_str": "1333966411467788288",
+		"quoted_status_permalink": {
+			"url": "https://t.co/vcVmJDwHCz",
+			"expanded": "https://twitter.com/kasal_finley/status/1333966411467788288",
+			"display": "twitter.com/kasal_finley/s…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Dec 02 02:49:11 +0000 2020",
+			"id": 1333966411467788300,
+			"id_str": "1333966411467788288",
+			"full_text": "I’ve been thinking a lot about my doctoral research (even though I did the research in 2011-2012). I studied school culture—and it’s something we just don’t talk enough about. \n\nI’ve been thinking about it as I see school leaders talk about how teachers shouldn’t complain \n1/",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				276
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1618463588,
+				"id_str": "1618463588",
+				"name": "Dawn Kasal Finley 🖤",
+				"screen_name": "kasal_finley",
+				"location": "",
+				"description": "Ph.D. | HS Teacher | Adj Professor | Widow 🖤 | Mom | Tweets are my own |Retweets are not endorsements | she/her | occupied Illini and Osage land",
+				"url": "https://t.co/qm5YQXMB89",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/qm5YQXMB89",
+								"expanded_url": "https://dawnfinley.wordpress.com/",
+								"display_url": "dawnfinley.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3568,
+				"friends_count": 1048,
+				"listed_count": 50,
+				"created_at": "Wed Jul 24 19:28:38 +0000 2013",
+				"favourites_count": 55318,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 15280,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "8C44C7",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1091559946758619141/JFk88bdN_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1091559946758619141/JFk88bdN_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1618463588/1548540698",
+				"profile_link_color": "981CEB",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 19,
+			"favorite_count": 198,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thread 👇 https://t.co/vcVmJDwHCz"
+	}
+]
