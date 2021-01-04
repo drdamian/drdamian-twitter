229357e97d04dd57d86517f928cac07acee8e93a
Damian Bariexca <damian@bariexca.net>
2021-01-03 20:11:23 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32837,
-	"created_at": "Sun Jan 03 01:11:22 +0000 2021",
+	"tweets": 32838,
+	"created_at": "Mon Jan 04 01:11:22 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sun Jan 03 16:17:15 +0000 2021",
+		"id": 1345766182385836000,
+		"id_str": "1345766182385836038",
+		"full_text": "25% done with Brother Robert, by Annye C. Anderson https://t.co/QgSm8YTnSD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QgSm8YTnSD",
+					"expanded_url": "https://goodreads.com/user_status/show/332353657",
+					"display_url": "goodreads.com/user_status/sh…",
+					"indices": [
+						51,
+						74
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.goodreads.com\" rel=\"nofollow\">Goodreads</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca is still wearing his mask 😷⛄",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal, @CardinalsLHS ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets & blogs ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/0mfonM3zAp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0mfonM3zAp",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3185,
+			"friends_count": 1557,
+			"listed_count": 246,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2413,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33046,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "25% done with Brother Robert, by Annye C. Anderson https://t.co/QgSm8YTnSD"
+	},
+	{
 		"created_at": "Sat Jan 02 16:48:01 +0000 2021",
 		"id": 1345411534558818300,
 		"id_str": "1345411534558818305",
@@ -110,7 +216,7 @@ Grailbird.data.tweets_2021_01 =
 		"created_at": "Sat Jan 02 16:20:54 +0000 2021",
 		"id": 1345404712372347000,
 		"id_str": "1345404712372346880",
-		"full_text": "@JenAnsbach Beats me.  At this rate he's gonna be older than me in a few years 🤷‍♂️",
+		"full_text": "@JenAnsbach Beats me.  At this rate he's gonna be older than me in a few years 🤷♂️",
 		"truncated": false,
 		"display_text_range": [
 			12,
@@ -210,7 +316,7 @@ Grailbird.data.tweets_2021_01 =
 		"favorited": false,
 		"retweeted": false,
 		"lang": "en",
-		"text": "@JenAnsbach Beats me.  At this rate he's gonna be older than me in a few years 🤷‍♂️"
+		"text": "@JenAnsbach Beats me.  At this rate he's gonna be older than me in a few years 🤷♂️"
 	},
 	{
 		"created_at": "Sat Jan 02 14:34:36 +0000 2021",
