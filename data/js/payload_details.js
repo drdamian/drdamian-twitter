0b459db6b469c557eb511783a8869c7eb3e6f910
Damian Bariexca <damian@bariexca.net>
2021-03-31 21:11:23 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32935,
-	"created_at": "Wed Mar 31 01:11:22 +0000 2021",
+	"tweets": 32936,
+	"created_at": "Thu Apr 01 01:11:21 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,292 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Wed Mar 31 16:16:51 +0000 2021",
+		"id": 1377293823756415000,
+		"id_str": "1377293823756414994",
+		"full_text": "Super glad my son started working at our local grocery store. https://t.co/6u48cCFIUW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6u48cCFIUW",
+					"expanded_url": "https://twitter.com/PAHealthDept/status/1377283111596257283",
+					"display_url": "twitter.com/PAHealthDept/s…",
+					"indices": [
+						62,
+						85
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca 💉💉😷",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal, @CardinalsLHS ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets & blogs ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/GQSCiLbWuN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GQSCiLbWuN",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3173,
+			"friends_count": 1558,
+			"listed_count": 245,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2477,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33178,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1377283111596257300,
+		"quoted_status_id_str": "1377283111596257283",
+		"quoted_status_permalink": {
+			"url": "https://t.co/6u48cCFIUW",
+			"expanded": "https://twitter.com/PAHealthDept/status/1377283111596257283",
+			"display": "twitter.com/PAHealthDept/s…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Mar 31 15:34:17 +0000 2021",
+			"id": 1377283111596257300,
+			"id_str": "1377283111596257283",
+			"full_text": "📢 PA COVID-19 VACCINATION ELIGIBILITY UPDATE! \n✅ Firefighters, police officers, grocery store workers and food and agriculture workers: eligible TODAY\n✅ Phase 1B: eligible April 5\n✅ Phase 1C: eligible April 12\n✅ All Pennsylvanians 16 and older: eligible April 19 https://t.co/3WcRl90NH4",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				262
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1377280310577131500,
+						"id_str": "1377280310577131520",
+						"indices": [
+							263,
+							286
+						],
+						"media_url": "http://pbs.twimg.com/media/Ex0VJTQW8AAU0w2.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ex0VJTQW8AAU0w2.jpg",
+						"url": "https://t.co/3WcRl90NH4",
+						"display_url": "pic.twitter.com/3WcRl90NH4",
+						"expanded_url": "https://twitter.com/PAHealthDept/status/1377283111596257283/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 356,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1200,
+								"h": 628,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 628,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1377280310577131500,
+						"id_str": "1377280310577131520",
+						"indices": [
+							263,
+							286
+						],
+						"media_url": "http://pbs.twimg.com/media/Ex0VJTQW8AAU0w2.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ex0VJTQW8AAU0w2.jpg",
+						"url": "https://t.co/3WcRl90NH4",
+						"display_url": "pic.twitter.com/3WcRl90NH4",
+						"expanded_url": "https://twitter.com/PAHealthDept/status/1377283111596257283/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 356,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1200,
+								"h": 628,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 628,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3218464527,
+				"id_str": "3218464527",
+				"name": "PA Department of Health",
+				"screen_name": "PAHealthDept",
+				"location": "Harrisburg, PA",
+				"description": "Official Twitter account for the Pennsylvania Department of Health. DOH is Pennsylvania’s primary source for public health information and services.",
+				"url": "https://t.co/8j11yVd6Dh",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/8j11yVd6Dh",
+								"expanded_url": "http://www.health.pa.gov",
+								"display_url": "health.pa.gov",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 91089,
+				"friends_count": 956,
+				"listed_count": 590,
+				"created_at": "Tue Apr 28 19:38:36 +0000 2015",
+				"favourites_count": 2246,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 16052,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1131895323918520322/YOury57e_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1131895323918520322/YOury57e_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3218464527/1616512110",
+				"profile_link_color": "89C9FA",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 203,
+			"favorite_count": 346,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Super glad my son started working at our local grocery store. https://t.co/6u48cCFIUW"
+	},
+	{
 		"created_at": "Wed Mar 31 01:04:21 +0000 2021",
 		"id": 1377064188472930300,
 		"id_str": "1377064188472930309",
