f2920d13d135eab0742dc85562dd3b12f56778d5
Damian Bariexca <damian@bariexca.net>
2021-04-02 21:11:22 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32936,
-	"created_at": "Thu Apr 01 01:11:21 +0000 2021",
+	"tweets": 32937,
+	"created_at": "Sat Apr 03 01:11:21 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_04.js
@@ -0,0 +1,389 @@
+Grailbird.data.tweets_2021_04 = 
+[
+	{
+		"created_at": "Fri Apr 02 16:01:39 +0000 2021",
+		"id": 1378014776085864400,
+		"id_str": "1378014776085864454",
+		"full_text": "Congratulations Dr. Milavsky! #finishEDD @LMSNation #LTPS https://t.co/UVuMoBipQj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			57
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "finishEDD",
+					"indices": [
+						30,
+						40
+					]
+				},
+				{
+					"text": "LTPS",
+					"indices": [
+						52,
+						57
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LMSNation",
+					"name": "LMS Nation",
+					"id": 309632193,
+					"id_str": "309632193",
+					"indices": [
+						41,
+						51
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/UVuMoBipQj",
+					"expanded_url": "https://twitter.com/mgcjusa/status/1378003807800602628",
+					"display_url": "twitter.com/mgcjusa/status…",
+					"indices": [
+						58,
+						81
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca 💉💉😷",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal, @CardinalsLHS ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets & blogs ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/GQSCiLbWuN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GQSCiLbWuN",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3174,
+			"friends_count": 1558,
+			"listed_count": 245,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2478,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33179,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1378003807800602600,
+		"quoted_status_id_str": "1378003807800602628",
+		"quoted_status_permalink": {
+			"url": "https://t.co/UVuMoBipQj",
+			"expanded": "https://twitter.com/mgcjusa/status/1378003807800602628",
+			"display": "twitter.com/mgcjusa/status…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Apr 02 15:18:04 +0000 2021",
+			"id": 1378003807800602600,
+			"id_str": "1378003807800602628",
+			"full_text": "Congratulations to Dr. Mindy Milavsky @mmilavsky on successfully defending her dissertation this morning.  Thanks so much to her committee @blehet and @DrMPancoast.  @RiderCEHS Enjoy the moment. https://t.co/BaSURBLtdF",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				194
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "mmilavsky",
+						"name": "Mindy",
+						"id": 2572760111,
+						"id_str": "2572760111",
+						"indices": [
+							38,
+							48
+						]
+					},
+					{
+						"screen_name": "blehet",
+						"name": "Dr. Bonnie Lehet",
+						"id": 92426289,
+						"id_str": "92426289",
+						"indices": [
+							139,
+							146
+						]
+					},
+					{
+						"screen_name": "DrMPancoast",
+						"name": "Mary Pancoast",
+						"id": 4048694559,
+						"id_str": "4048694559",
+						"indices": [
+							151,
+							163
+						]
+					},
+					{
+						"screen_name": "RiderCEHS",
+						"name": "Rider College of Education & Human Services",
+						"id": 2364392924,
+						"id_str": "2364392924",
+						"indices": [
+							166,
+							176
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1378003802750603300,
+						"id_str": "1378003802750603265",
+						"indices": [
+							195,
+							218
+						],
+						"media_url": "http://pbs.twimg.com/media/Ex-nKFoWUAEg3Pe.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ex-nKFoWUAEg3Pe.jpg",
+						"url": "https://t.co/BaSURBLtdF",
+						"display_url": "pic.twitter.com/BaSURBLtdF",
+						"expanded_url": "https://twitter.com/mgcjusa/status/1378003807800602628/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1378003802750603300,
+						"id_str": "1378003802750603265",
+						"indices": [
+							195,
+							218
+						],
+						"media_url": "http://pbs.twimg.com/media/Ex-nKFoWUAEg3Pe.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ex-nKFoWUAEg3Pe.jpg",
+						"url": "https://t.co/BaSURBLtdF",
+						"display_url": "pic.twitter.com/BaSURBLtdF",
+						"expanded_url": "https://twitter.com/mgcjusa/status/1378003807800602628/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 21947812,
+				"id_str": "21947812",
+				"name": "Michael G Curran Jr",
+				"screen_name": "mgcjusa",
+				"location": "Lawrenceville, NJ",
+				"description": "Teacher Ed-Rider U; Google Ed Level 1&2; Educators Voice Prof of the Year, 2015; former HS T, NJDOE Admin; NBEA PP. 3-time marathoner. Solely my comments.",
+				"url": "https://t.co/lOs5ops9FC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/lOs5ops9FC",
+								"expanded_url": "http://mgcjusa.weebly.com",
+								"display_url": "mgcjusa.weebly.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6715,
+				"friends_count": 4678,
+				"listed_count": 568,
+				"created_at": "Thu Feb 26 00:46:55 +0000 2009",
+				"favourites_count": 91993,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 122381,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1328707999016349697/UtunU4k7_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1328707999016349697/UtunU4k7_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/21947812/1615593234",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "004fcac6274ef9ca",
+				"url": "https://api.twitter.com/1.1/geo/id/004fcac6274ef9ca.json",
+				"place_type": "city",
+				"name": "Lawrence",
+				"full_name": "Lawrence, NJ",
+				"country_code": "US",
+				"country": "United States",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-74.762755,
+								40.2398165
+							],
+							[
+								-74.6644118,
+								40.2398165
+							],
+							[
+								-74.6644118,
+								40.3177474
+							],
+							[
+								-74.762755,
+								40.3177474
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 25,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Congratulations Dr. Milavsky! #finishEDD @LMSNation #LTPS https://t.co/UVuMoBipQj"
+	}
+]
