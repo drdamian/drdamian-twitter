4475b54405164d645916f436146a39fc9628330d
Damian Bariexca <damian@bariexca.net>
2021-04-19 21:11:22 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32949,
-	"created_at": "Sun Apr 18 01:11:21 +0000 2021",
+	"tweets": 32950,
+	"created_at": "Tue Apr 20 01:11:21 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Tue Apr 20 00:37:15 +0000 2021",
+		"id": 1384303412171329500,
+		"id_str": "1384305125544501259",
+		"full_text": "#DECAICDC Opening Session has officially begun! ⁦Here we go ⁦@CardinalsLHS⁩ #DECA! #DECAINFINITY ⁦@dadamltps⁩ ⁦@_drdamian⁩ https://t.co/FVtnX1LYyu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DECAICDC",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"text": "DECA",
+					"indices": [
+						76,
+						81
+					]
+				},
+				{
+					"text": "DECAINFINITY",
+					"indices": [
+						83,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CardinalsLHS",
+					"name": "Lawrence High School",
+					"id": 832085833,
+					"id_str": "832085833",
+					"indices": [
+						61,
+						74
+					]
+				},
+				{
+					"screen_name": "dadamltps",
+					"name": "David Adam",
+					"id": 260753316,
+					"id_str": "260753316",
+					"indices": [
+						98,
+						108
+					]
+				},
+				{
+					"screen_name": "_drdamian",
+					"name": "Dr. Damian Bariexca 💉💉😷",
+					"id": 7887852,
+					"id_str": "7887852",
+					"indices": [
+						111,
+						121
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1384303405716316200,
+					"id_str": "1384303405716316166",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/EzYInPcVoAYcWrO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EzYInPcVoAYcWrO.jpg",
+					"url": "https://t.co/FVtnX1LYyu",
+					"display_url": "pic.twitter.com/FVtnX1LYyu",
+					"expanded_url": "https://twitter.com/john_tees/status/1384303412171329539/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2016,
+							"h": 1512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1384303405716316200,
+					"id_str": "1384303405716316166",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/EzYInPcVoAYcWrO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EzYInPcVoAYcWrO.jpg",
+					"url": "https://t.co/FVtnX1LYyu",
+					"display_url": "pic.twitter.com/FVtnX1LYyu",
+					"expanded_url": "https://twitter.com/john_tees/status/1384303412171329539/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2016,
+							"h": 1512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 801118345,
+			"id_str": "801118345",
+			"name": "John Tees",
+			"screen_name": "john_tees",
+			"location": "",
+			"description": "High School Business Educator and Co-DECA Advisor in Lawrenceville, NJ. Solely my comments.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 132,
+			"friends_count": 130,
+			"listed_count": 1,
+			"created_at": "Mon Sep 03 20:03:32 +0000 2012",
+			"favourites_count": 430,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 214,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1103658353404850176/Asky8PI__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1103658353404850176/Asky8PI__normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 5,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#DECAICDC Opening Session has officially begun! ⁦Here we go ⁦@CardinalsLHS⁩ #DECA! #DECAINFINITY ⁦@dadamltps⁩ ⁦@_drdamian⁩ https://t.co/FVtnX1LYyu"
+	},
+	{
 		"created_at": "Sat Apr 17 19:44:29 +0000 2021",
 		"id": 1383506673378074600,
 		"id_str": "1383506673378074627",
