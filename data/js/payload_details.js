40e9e61eea5f172c242abd6bc028d1b7e8166597
Damian Bariexca <damian@bariexca.net>
2021-04-21 21:11:22 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32950,
-	"created_at": "Tue Apr 20 01:11:21 +0000 2021",
+	"tweets": 32951,
+	"created_at": "Thu Apr 22 01:11:21 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,10 +1,117 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Wed Apr 21 10:56:13 +0000 2021",
+		"id": 1384823278938820600,
+		"id_str": "1384823278938820608",
+		"full_text": "25% done with Kill 'Em and Leave, by James McBride https://t.co/QLMcbg1Mc9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QLMcbg1Mc9",
+					"expanded_url": "https://goodreads.com/user_status/show/365404331",
+					"display_url": "goodreads.com/user_status/sh…",
+					"indices": [
+						51,
+						74
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.goodreads.com\" rel=\"nofollow\">Goodreads</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca 💉💉😷",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal, @CardinalsLHS ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets & blogs ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/GQSCiLbWuN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GQSCiLbWuN",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3169,
+			"friends_count": 1560,
+			"listed_count": 245,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2499,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33198,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "25% done with Kill 'Em and Leave, by James McBride https://t.co/QLMcbg1Mc9"
+	},
+	{
 		"created_at": "Tue Apr 20 00:37:15 +0000 2021",
 		"id": 1384303412171329500,
 		"id_str": "1384305125544501259",
-		"full_text": "#DECAICDC Opening Session has officially begun! ⁦Here we go ⁦@CardinalsLHS⁩ #DECA! #DECAINFINITY ⁦@dadamltps⁩ ⁦@_drdamian⁩ https://t.co/FVtnX1LYyu",
+		"full_text": "#DECAICDC Opening Session has officially begun! Here we go @CardinalsLHS #DECA! #DECAINFINITY @dadamltps @_drdamian https://t.co/FVtnX1LYyu",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -212,7 +319,7 @@ Grailbird.data.tweets_2021_04 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "#DECAICDC Opening Session has officially begun! ⁦Here we go ⁦@CardinalsLHS⁩ #DECA! #DECAINFINITY ⁦@dadamltps⁩ ⁦@_drdamian⁩ https://t.co/FVtnX1LYyu"
+		"text": "#DECAICDC Opening Session has officially begun! Here we go @CardinalsLHS #DECA! #DECAINFINITY @dadamltps @_drdamian https://t.co/FVtnX1LYyu"
 	},
 	{
 		"created_at": "Sat Apr 17 19:44:29 +0000 2021",
