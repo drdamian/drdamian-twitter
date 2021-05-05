4c6036b6f98fdf7d357bf4397a2ecf723a941470
Damian Bariexca <damian@bariexca.net>
2021-05-04 21:11:21 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32962,
-	"created_at": "Tue May 04 01:11:22 +0000 2021",
+	"tweets": 32963,
+	"created_at": "Wed May 05 01:11:21 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Tue May 04 12:47:20 +0000 2021",
+		"id": 1389562284683432000,
+		"id_str": "1389562284683431949",
+		"full_text": "@Glennr1809 https://t.co/crECjVGhpP",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			11
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Glennr1809",
+					"name": "Glenn Robbins",
+					"id": 153925839,
+					"id_str": "153925839",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1389562278861844500,
+					"id_str": "1389562278861844480",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/E0i3h8jXsAA_qah.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/E0i3h8jXsAA_qah.jpg",
+					"url": "https://t.co/crECjVGhpP",
+					"display_url": "pic.twitter.com/crECjVGhpP",
+					"expanded_url": "https://twitter.com/_drdamian/status/1389562284683431949/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 356,
+							"h": 296,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 356,
+							"h": 296,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 356,
+							"h": 296,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1389562278861844500,
+					"id_str": "1389562278861844480",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/E0i3h8jXsAA_qah.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/E0i3h8jXsAA_qah.jpg",
+					"url": "https://t.co/crECjVGhpP",
+					"display_url": "pic.twitter.com/crECjVGhpP",
+					"expanded_url": "https://twitter.com/_drdamian/status/1389562284683431949/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"small": {
+							"w": 356,
+							"h": 296,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 356,
+							"h": 296,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 356,
+							"h": 296,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							89,
+							74
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/E0i3h8jXsAA_qah.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1389529565631033300,
+		"in_reply_to_status_id_str": "1389529565631033346",
+		"in_reply_to_user_id": 153925839,
+		"in_reply_to_user_id_str": "153925839",
+		"in_reply_to_screen_name": "Glennr1809",
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca 💉💉😷",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal, @CardinalsLHS ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets & blogs ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/GQSCiLbWuN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GQSCiLbWuN",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3168,
+			"friends_count": 1561,
+			"listed_count": 245,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2508,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33217,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@Glennr1809 https://t.co/crECjVGhpP"
+	},
+	{
 		"created_at": "Mon May 03 22:03:03 +0000 2021",
 		"id": 1389337247418290200,
 		"id_str": "1389339750159593472",
