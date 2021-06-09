5652b6c600640de92ca50fd4ac678a57a5662e5e
Damian Bariexca <damian@bariexca.net>
2021-06-08 21:11:22 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32985,
-	"created_at": "Tue Jun 08 01:11:21 +0000 2021",
+	"tweets": 32986,
+	"created_at": "Wed Jun 09 01:11:21 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,341 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Wed Jun 09 00:52:47 +0000 2021",
+		"id": 1402428426313678800,
+		"id_str": "1402428426313678853",
+		"full_text": "🧵👇 Venmo is a great tool I use often, but the whole world doesn't need to know why my friends and I (or you and yours) are giving or receiving money. Hope you find this useful: https://t.co/RXzxjZHzS6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			176
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RXzxjZHzS6",
+					"expanded_url": "https://twitter.com/EFF/status/1402417933876273154",
+					"display_url": "twitter.com/EFF/status/140…",
+					"indices": [
+						177,
+						200
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal, @CardinalsLHS ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets ≠ opinion of my employers ~ he/him/his",
+			"url": "https://t.co/GQSCiLbWuN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GQSCiLbWuN",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3146,
+			"friends_count": 1561,
+			"listed_count": 245,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2513,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33250,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1400548953",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1402417933876273200,
+		"quoted_status_id_str": "1402417933876273154",
+		"quoted_status_permalink": {
+			"url": "https://t.co/RXzxjZHzS6",
+			"expanded": "https://twitter.com/EFF/status/1402417933876273154",
+			"display": "twitter.com/EFF/status/140…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jun 09 00:11:05 +0000 2021",
+			"id": 1402417933876273200,
+			"id_str": "1402417933876273154",
+			"full_text": "At last, we've got the option to make our friends list private in @Venmo. Here's how to lock down your account, in 8 quick steps:\n\n1: Tap the three lines in the top right corner, select Settings near the bottom. From the settings screen, select Privacy, then Friends List. https://t.co/nIeglORQFD",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				272
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Venmo",
+						"name": "Venmo",
+						"id": 18580938,
+						"id_str": "18580938",
+						"indices": [
+							66,
+							72
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1402407114455146500,
+						"id_str": "1402407114455146497",
+						"indices": [
+							273,
+							296
+						],
+						"media_url": "http://pbs.twimg.com/media/E3ZZ1x-UYAELg43.png",
+						"media_url_https": "https://pbs.twimg.com/media/E3ZZ1x-UYAELg43.png",
+						"url": "https://t.co/nIeglORQFD",
+						"display_url": "pic.twitter.com/nIeglORQFD",
+						"expanded_url": "https://twitter.com/EFF/status/1402417933876273154/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 254,
+								"h": 480,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 254,
+								"h": 480,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 254,
+								"h": 480,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1402407114455146500,
+						"id_str": "1402407114455146497",
+						"indices": [
+							273,
+							296
+						],
+						"media_url": "http://pbs.twimg.com/media/E3ZZ1x-UYAELg43.png",
+						"media_url_https": "https://pbs.twimg.com/media/E3ZZ1x-UYAELg43.png",
+						"url": "https://t.co/nIeglORQFD",
+						"display_url": "pic.twitter.com/nIeglORQFD",
+						"expanded_url": "https://twitter.com/EFF/status/1402417933876273154/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 254,
+								"h": 480,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 254,
+								"h": 480,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 254,
+								"h": 480,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1402407635706515500,
+						"id_str": "1402407635706515456",
+						"indices": [
+							273,
+							296
+						],
+						"media_url": "http://pbs.twimg.com/media/E3ZaUHyVEAANvM8.png",
+						"media_url_https": "https://pbs.twimg.com/media/E3ZaUHyVEAANvM8.png",
+						"url": "https://t.co/nIeglORQFD",
+						"display_url": "pic.twitter.com/nIeglORQFD",
+						"expanded_url": "https://twitter.com/EFF/status/1402417933876273154/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 254,
+								"h": 480,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 254,
+								"h": 480,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 254,
+								"h": 480,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 4816,
+				"id_str": "4816",
+				"name": "EFF",
+				"screen_name": "EFF",
+				"location": "San Francisco, CA",
+				"description": "We're the Electronic Frontier Foundation. We defend your civil liberties in a digital world.",
+				"url": "https://t.co/1gDghboN0q",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/1gDghboN0q",
+								"expanded_url": "https://www.eff.org",
+								"display_url": "eff.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 476666,
+				"friends_count": 1059,
+				"listed_count": 14146,
+				"created_at": "Mon Aug 28 14:17:28 +0000 2006",
+				"favourites_count": 2236,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 31023,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "E17701",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1109145060929490945/H1RmsjEu_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1109145060929490945/H1RmsjEu_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/4816/1610043025",
+				"profile_link_color": "6A3ACB",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E8D8DD",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 57,
+			"favorite_count": 64,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🧵👇 Venmo is a great tool I use often, but the whole world doesn't need to know why my friends and I (or you and yours) are giving or receiving money. Hope you find this useful: https://t.co/RXzxjZHzS6"
+	},
+	{
 		"created_at": "Mon Jun 07 18:46:58 +0000 2021",
 		"id": 1401898483288838100,
 		"id_str": "1401973980324667394",
