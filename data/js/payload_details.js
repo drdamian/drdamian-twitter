2b90b457af39d8961059d73d3b885c2429f74313
Damian Bariexca <damian@bariexca.net>
2021-12-02 20:11:21 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33079,
-	"created_at": "Mon Nov 29 01:11:22 +0000 2021",
+	"tweets": 33080,
+	"created_at": "Fri Dec 03 01:11:21 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_12.js
@@ -0,0 +1,267 @@
+Grailbird.data.tweets_2021_12 = 
+[
+	{
+		"created_at": "Thu Dec 02 01:40:44 +0000 2021",
+		"id": 1466141425490403300,
+		"id_str": "1466220756568887298",
+		"full_text": "International Foods made a stop in Italy today and had so much fun! Chicken Parmigiana…yum! @CardinalsLHS @LTPS1 @_drdamian https://t.co/LppgJETeNj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CardinalsLHS",
+					"name": "Lawrence High School",
+					"id": 832085833,
+					"id_str": "832085833",
+					"indices": [
+						92,
+						105
+					]
+				},
+				{
+					"screen_name": "LTPS1",
+					"name": "Lawrence Township Public Schools",
+					"id": 2178717536,
+					"id_str": "2178717536",
+					"indices": [
+						106,
+						112
+					]
+				},
+				{
+					"screen_name": "_drdamian",
+					"name": "Dr. Damian Bariexca",
+					"id": 7887852,
+					"id_str": "7887852",
+					"indices": [
+						113,
+						123
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1466141421010886700,
+					"id_str": "1466141421010886662",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/FFjH2r3XIAYR0ov.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFjH2r3XIAYR0ov.jpg",
+					"url": "https://t.co/LppgJETeNj",
+					"display_url": "pic.twitter.com/LppgJETeNj",
+					"expanded_url": "https://twitter.com/MrsCassidy_FCS/status/1466141425490403332/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1466141421010886700,
+					"id_str": "1466141421010886662",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/FFjH2r3XIAYR0ov.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFjH2r3XIAYR0ov.jpg",
+					"url": "https://t.co/LppgJETeNj",
+					"display_url": "pic.twitter.com/LppgJETeNj",
+					"expanded_url": "https://twitter.com/MrsCassidy_FCS/status/1466141425490403332/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1466141421015076900,
+					"id_str": "1466141421015076865",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/FFjH2r4XEAEi2o5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFjH2r4XEAEi2o5.jpg",
+					"url": "https://t.co/LppgJETeNj",
+					"display_url": "pic.twitter.com/LppgJETeNj",
+					"expanded_url": "https://twitter.com/MrsCassidy_FCS/status/1466141425490403332/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1466141421203775500,
+					"id_str": "1466141421203775505",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/FFjH2slWYBE8MPu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFjH2slWYBE8MPu.jpg",
+					"url": "https://t.co/LppgJETeNj",
+					"display_url": "pic.twitter.com/LppgJETeNj",
+					"expanded_url": "https://twitter.com/MrsCassidy_FCS/status/1466141425490403332/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1461831257881555000,
+			"id_str": "1461831257881554944",
+			"name": "Wendi Cassidy",
+			"screen_name": "MrsCassidy_FCS",
+			"location": "",
+			"description": "FCS Teacher at LHS",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5,
+			"friends_count": 21,
+			"listed_count": 0,
+			"created_at": "Fri Nov 19 22:58:37 +0000 2021",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1461831533984194571/ns0aJjnZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1461831533984194571/ns0aJjnZ_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "International Foods made a stop in Italy today and had so much fun! Chicken Parmigiana…yum! @CardinalsLHS @LTPS1 @_drdamian https://t.co/LppgJETeNj"
+	}
+]
