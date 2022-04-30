5c438c2a772c17d42ba3e615ce9b6997cf27be64
Damian Bariexca <damian@bariexca.net>
2022-04-29 21:11:22 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33148,
-	"created_at": "Wed Apr 27 01:11:21 +0000 2022",
+	"tweets": 33149,
+	"created_at": "Sat Apr 30 01:11:21 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,247 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Fri Apr 29 21:21:04 +0000 2022",
+		"id": 1520018878776156200,
+		"id_str": "1520151203845390338",
+		"full_text": "We feel appreciated! Thank you @dadamltps @cjwilliamsjr @_drdamian @Brenda_Eke #SchoolLibraryMonth @NJASL #notyourparentslibrary https://t.co/QzwM8T4tXb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SchoolLibraryMonth",
+					"indices": [
+						79,
+						98
+					]
+				},
+				{
+					"text": "notyourparentslibrary",
+					"indices": [
+						106,
+						128
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dadamltps",
+					"name": "David Adam, Ed. D.",
+					"id": 260753316,
+					"id_str": "260753316",
+					"indices": [
+						31,
+						41
+					]
+				},
+				{
+					"screen_name": "cjwilliamsjr",
+					"name": "Cliff Williams",
+					"id": 465193444,
+					"id_str": "465193444",
+					"indices": [
+						42,
+						55
+					]
+				},
+				{
+					"screen_name": "_drdamian",
+					"name": "Dr. Damian Bariexca",
+					"id": 7887852,
+					"id_str": "7887852",
+					"indices": [
+						56,
+						66
+					]
+				},
+				{
+					"screen_name": "Brenda_Eke",
+					"name": "Brenda Eke",
+					"id": 1234921057666183200,
+					"id_str": "1234921057666183168",
+					"indices": [
+						67,
+						78
+					]
+				},
+				{
+					"screen_name": "NJASL",
+					"name": "NJ Assn School Libns",
+					"id": 87832393,
+					"id_str": "87832393",
+					"indices": [
+						99,
+						105
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1520018872442798000,
+					"id_str": "1520018872442798082",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/FRgxHG_XwAIRbs3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRgxHG_XwAIRbs3.jpg",
+					"url": "https://t.co/QzwM8T4tXb",
+					"display_url": "pic.twitter.com/QzwM8T4tXb",
+					"expanded_url": "https://twitter.com/LHSMediaCenter/status/1520018878776156162/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1520018872442798000,
+					"id_str": "1520018872442798082",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/FRgxHG_XwAIRbs3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRgxHG_XwAIRbs3.jpg",
+					"url": "https://t.co/QzwM8T4tXb",
+					"display_url": "pic.twitter.com/QzwM8T4tXb",
+					"expanded_url": "https://twitter.com/LHSMediaCenter/status/1520018878776156162/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 796644332,
+			"id_str": "796644332",
+			"name": "Ewa Dziedzic-Elliott",
+			"screen_name": "LHSMediaCenter",
+			"location": "Lawrenceville, NJ",
+			"description": "Lawrence High School Media Center Specialist/Librarian, aspiring administrator, educational leader, change maker, @NJASL President Elect, 2021 Conference Chair.",
+			"url": "https://t.co/M4rV6FJ06t",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/M4rV6FJ06t",
+							"expanded_url": "http://lawrence1.schoolwires.net/Page/2757",
+							"display_url": "lawrence1.schoolwires.net/Page/2757",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 705,
+			"friends_count": 1383,
+			"listed_count": 22,
+			"created_at": "Sat Sep 01 18:42:52 +0000 2012",
+			"favourites_count": 5617,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1921,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "660000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1497034771423191043/t6Oc8UUj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1497034771423191043/t6Oc8UUj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/796644332/1650927758",
+			"profile_link_color": "660000",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 8,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We feel appreciated! Thank you @dadamltps @cjwilliamsjr @_drdamian @Brenda_Eke #SchoolLibraryMonth @NJASL #notyourparentslibrary https://t.co/QzwM8T4tXb"
+	},
+	{
 		"created_at": "Tue Apr 26 23:09:37 +0000 2022",
 		"id": 1519091356920168400,
 		"id_str": "1519091356920168448",
