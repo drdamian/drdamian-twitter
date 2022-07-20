fe6ce71e639781ad2ee9c90e91cab8e7bea6c820
Damian Bariexca <damian@bariexca.net>
2022-07-19 21:11:23 -0400

    Added 1 tweets

--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Tue Jul 19 23:43:37 +0000 2022",
+		"id": 1549540495252152300,
+		"id_str": "1549540495252152321",
+		"full_text": "Now just hoping for US tour dates... 🤞 https://t.co/qiVseuBhrt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			38
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qiVseuBhrt",
+					"expanded_url": "https://twitter.com/Sloanmusic/status/1547237293987172354",
+					"display_url": "twitter.com/Sloanmusic/sta…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Dr. Damian Bariexca",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal & Instructional Supervisor, @CardinalsLHS ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets ≠ opinion of my employers ~ he/him",
+			"url": "https://t.co/GQSCiLtxml",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GQSCiLtxml",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3109,
+			"friends_count": 1545,
+			"listed_count": 240,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2581,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33493,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1657972223",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1547237293987172400,
+		"quoted_status_id_str": "1547237293987172354",
+		"quoted_status_permalink": {
+			"url": "https://t.co/qiVseuBhrt",
+			"expanded": "https://twitter.com/Sloanmusic/status/1547237293987172354",
+			"display": "twitter.com/Sloanmusic/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jul 13 15:11:31 +0000 2022",
+			"id": 1547237293987172400,
+			"id_str": "1547237293987172354",
+			"full_text": "New album Sloan Steady out October 21st (30 years since debut LP!) and new song \"Spend The Day\" streaming now! \nListen to Spend The Day : https://t.co/kraf0S90U4\nPre-order vinyl &amp; CD : https://t.co/f8ZivLB9tn",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				212
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/kraf0S90U4",
+						"expanded_url": "https://sloan.lnk.tt/Steady",
+						"display_url": "sloan.lnk.tt/Steady",
+						"indices": [
+							138,
+							161
+						]
+					},
+					{
+						"url": "https://t.co/f8ZivLB9tn",
+						"expanded_url": "https://shop.udiscovermusic.ca/search?q=sloan+steady",
+						"display_url": "shop.udiscovermusic.ca/search?q=sloan…",
+						"indices": [
+							189,
+							212
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 22154398,
+				"id_str": "22154398",
+				"name": "Sloan",
+				"screen_name": "Sloanmusic",
+				"location": "Toronto",
+				"description": "1991 - 2022....",
+				"url": "https://t.co/xlClEqLRgE",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/xlClEqLRgE",
+								"expanded_url": "https://linktr.ee/Sloanmusic",
+								"display_url": "linktr.ee/Sloanmusic",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 39816,
+				"friends_count": 7308,
+				"listed_count": 721,
+				"created_at": "Fri Feb 27 16:04:35 +0000 2009",
+				"favourites_count": 98,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 4721,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "450C87",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1547236973190021120/UhBNHaI1_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1547236973190021120/UhBNHaI1_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/22154398/1657289805",
+				"profile_link_color": "7C11BA",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "F2D12C",
+				"profile_text_color": "343485",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 31,
+			"favorite_count": 113,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Now just hoping for US tour dates... 🤞 https://t.co/qiVseuBhrt"
+	},
+	{
 		"created_at": "Mon Jul 18 00:14:18 +0000 2022",
 		"id": 1548823441347952600,
 		"id_str": "1548823441347952642",
