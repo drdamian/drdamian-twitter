bd3151a611f96934c9fc9d4c4ed07ef2d0343758
Damian Bariexca <damian@bariexca.net>
2023-03-12 21:11:22 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33300,
-	"created_at": "Sat Mar 11 01:11:21 +0000 2023",
+	"tweets": 33301,
+	"created_at": "Mon Mar 13 01:11:21 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,375 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Sun Mar 12 22:25:39 +0000 2023",
+		"id": 1635044409170600000,
+		"id_str": "1635044409170599941",
+		"full_text": "Looking good, doc - congratulations on a job well done! #LevelUpLHS #LTPS https://t.co/JnYs7Mehos",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			73
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LevelUpLHS",
+					"indices": [
+						56,
+						67
+					]
+				},
+				{
+					"text": "LTPS",
+					"indices": [
+						68,
+						73
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JnYs7Mehos",
+					"expanded_url": "https://twitter.com/dadamltps/status/1634594948510101504",
+					"display_url": "twitter.com/dadamltps/stat…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Damian N. Bariexca, Ed.D.",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal & Instructional Supervisor, @CardinalsLHS ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets ≠ opinion of my employers ~ he/him",
+			"url": "https://t.co/GQSCiLbWuN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GQSCiLbWuN",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3012,
+			"friends_count": 1507,
+			"listed_count": 228,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2595,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33626,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1657972223",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1634594948510101500,
+		"quoted_status_id_str": "1634594948510101504",
+		"quoted_status_permalink": {
+			"url": "https://t.co/JnYs7Mehos",
+			"expanded": "https://twitter.com/dadamltps/status/1634594948510101504",
+			"display": "twitter.com/dadamltps/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Mar 11 16:39:39 +0000 2023",
+			"id": 1634594948510101500,
+			"id_str": "1634594948510101504",
+			"full_text": "We are so fortunate to be a part of this awesome @StBaldricks organization... let's keep raising funds and cure childhood cancer!!! Great first night by the @CardinalsLHS team. Thanks to all our @LTPS1 supporters... @rtmurphy331 #LevelupLHS https://t.co/mYxd7EyEEq https://t.co/9055ugmHr9",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				264
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "LevelupLHS",
+						"indices": [
+							229,
+							240
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "StBaldricks",
+						"name": "St. Baldrick's Foundation",
+						"id": 29249435,
+						"id_str": "29249435",
+						"indices": [
+							49,
+							61
+						]
+					},
+					{
+						"screen_name": "CardinalsLHS",
+						"name": "Lawrence High School",
+						"id": 832085833,
+						"id_str": "832085833",
+						"indices": [
+							157,
+							170
+						]
+					},
+					{
+						"screen_name": "LTPS1",
+						"name": "Lawrence Township Public Schools",
+						"id": 2178717536,
+						"id_str": "2178717536",
+						"indices": [
+							195,
+							201
+						]
+					},
+					{
+						"screen_name": "rtmurphy331",
+						"name": "Ray Murphy",
+						"id": 2451285449,
+						"id_str": "2451285449",
+						"indices": [
+							216,
+							228
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/mYxd7EyEEq",
+						"expanded_url": "https://twitter.com/robyn_klim/status/1634393128923701249",
+						"display_url": "twitter.com/robyn_klim/sta…",
+						"indices": [
+							241,
+							264
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1634594945804771300,
+						"id_str": "1634594945804771328",
+						"indices": [
+							265,
+							288
+						],
+						"media_url": "http://pbs.twimg.com/media/Fq8_cVhXoAAWsv0.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Fq8_cVhXoAAWsv0.jpg",
+						"url": "https://t.co/9055ugmHr9",
+						"display_url": "pic.twitter.com/9055ugmHr9",
+						"expanded_url": "https://twitter.com/dadamltps/status/1634594948510101504/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 729,
+								"h": 972,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 729,
+								"h": 972,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1634594945804771300,
+						"id_str": "1634594945804771328",
+						"indices": [
+							265,
+							288
+						],
+						"media_url": "http://pbs.twimg.com/media/Fq8_cVhXoAAWsv0.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Fq8_cVhXoAAWsv0.jpg",
+						"url": "https://t.co/9055ugmHr9",
+						"display_url": "pic.twitter.com/9055ugmHr9",
+						"expanded_url": "https://twitter.com/dadamltps/status/1634594948510101504/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 729,
+								"h": 972,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 729,
+								"h": 972,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 260753316,
+				"id_str": "260753316",
+				"name": "David Adam, Ed. D.",
+				"screen_name": "dadamltps",
+				"location": "Lawrence, NJ",
+				"description": "Principal of Lawrence High School",
+				"url": "https://t.co/CVxJlThsNi",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/CVxJlThsNi",
+								"expanded_url": "http://www.ltps.org",
+								"display_url": "ltps.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1049,
+				"friends_count": 166,
+				"listed_count": 39,
+				"created_at": "Fri Mar 04 14:40:19 +0000 2011",
+				"favourites_count": 18094,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5161,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/751086341094539264/mfshfF8w_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/751086341094539264/mfshfF8w_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/260753316/1477665508",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1634393128923701200,
+			"quoted_status_id_str": "1634393128923701249",
+			"quoted_status_permalink": {
+				"url": "https://t.co/mYxd7EyEEq",
+				"expanded": "https://twitter.com/robyn_klim/status/1634393128923701249",
+				"display": "twitter.com/robyn_klim/sta…"
+			},
+			"retweet_count": 0,
+			"favorite_count": 9,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Looking good, doc - congratulations on a job well done! #LevelUpLHS #LTPS https://t.co/JnYs7Mehos"
+	},
+	{
 		"created_at": "Fri Mar 10 03:16:18 +0000 2023",
 		"id": 1634030391651709000,
 		"id_str": "1634030391651708928",
