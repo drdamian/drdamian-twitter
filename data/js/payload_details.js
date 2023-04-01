ecadfc4183ba0df2e6953b77297aa58e7042c0bd
Damian Bariexca <damian@bariexca.net>
2023-03-31 21:11:23 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33302,
-	"created_at": "Sun Mar 19 01:11:21 +0000 2023",
+	"tweets": 33303,
+	"created_at": "Sat Apr 01 01:11:22 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Fri Mar 31 22:52:44 +0000 2023",
+		"id": 1641936593693835300,
+		"id_str": "1641936593693835265",
+		"full_text": "So proud of my son Dylan! After a lengthy application process and acceptances at multiple schools, he will be attending Fairleigh Dickinson University next year as a biochemistry major in the Honors Academy.  The FDU Honors Academy only accepts 10 students per campus per year! https://t.co/9affnq9B96",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1641936577176694800,
+					"id_str": "1641936577176694785",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FslUnhpWcAEZaKH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FslUnhpWcAEZaKH.jpg",
+					"url": "https://t.co/9affnq9B96",
+					"display_url": "pic.twitter.com/9affnq9B96",
+					"expanded_url": "https://twitter.com/_drdamian/status/1641936593693835265/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 843,
+							"h": 843,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 843,
+							"h": 843,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1641936577176694800,
+					"id_str": "1641936577176694785",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FslUnhpWcAEZaKH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FslUnhpWcAEZaKH.jpg",
+					"url": "https://t.co/9affnq9B96",
+					"display_url": "pic.twitter.com/9affnq9B96",
+					"expanded_url": "https://twitter.com/_drdamian/status/1641936593693835265/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 843,
+							"h": 843,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 843,
+							"h": 843,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Damian N. Bariexca, Ed.D.",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal & Instructional Supervisor, @CardinalsLHS ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets ≠ opinion of my employers ~ he/him",
+			"url": "https://t.co/GQSCiLbWuN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GQSCiLbWuN",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3004,
+			"friends_count": 1504,
+			"listed_count": 228,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2596,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33629,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1657972223",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "So proud of my son Dylan! After a lengthy application process and acceptances at multiple schools, he will be attending Fairleigh Dickinson University next year as a biochemistry major in the Honors Academy.  The FDU Honors Academy only accepts 10 students per campus per year! https://t.co/9affnq9B96"
+	},
+	{
 		"created_at": "Sat Mar 18 12:26:07 +0000 2023",
 		"id": 1635979568170270700,
 		"id_str": "1637067859946876930",
