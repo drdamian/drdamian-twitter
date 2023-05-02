d6d713cb141f2cded872001f78fee4337358aa54
Damian Bariexca <damian@bariexca.net>
2023-05-01 21:11:27 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33317,
-	"created_at": "Thu Apr 27 01:11:23 +0000 2023",
+	"tweets": 33318,
+	"created_at": "Tue May 02 01:11:26 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_05.js
@@ -0,0 +1,378 @@
+Grailbird.data.tweets_2023_05 = 
+[
+	{
+		"created_at": "Mon May 01 21:22:42 +0000 2023",
+		"id": 1653147960597786600,
+		"id_str": "1653147960597786630",
+		"full_text": "Happy National School Principals' Day to all our #LTPS principals: @MNLockett @MuziLearningLab @JayBilly2 @AmyAmiet @drafischer @LMSNation and the very proud principal of LHS, @dadamltps!  We appreciate all you do for our students! https://t.co/VuQGx20riO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			231
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LTPS",
+					"indices": [
+						49,
+						54
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MNLockett",
+					"name": "Melissa Lockett",
+					"id": 297397894,
+					"id_str": "297397894",
+					"indices": [
+						67,
+						77
+					]
+				},
+				{
+					"screen_name": "MuziLearningLab",
+					"name": "Jeanne Muzi",
+					"id": 1618799258,
+					"id_str": "1618799258",
+					"indices": [
+						78,
+						94
+					]
+				},
+				{
+					"screen_name": "JayBilly2",
+					"name": "Jay Billy",
+					"id": 1455489762622595000,
+					"id_str": "1455489762622595072",
+					"indices": [
+						95,
+						105
+					]
+				},
+				{
+					"screen_name": "AmyAmiet",
+					"name": "Amy Amiet",
+					"id": 514809785,
+					"id_str": "514809785",
+					"indices": [
+						106,
+						115
+					]
+				},
+				{
+					"screen_name": "drafischer",
+					"name": "Alyson Fischer",
+					"id": 720423019,
+					"id_str": "720423019",
+					"indices": [
+						116,
+						127
+					]
+				},
+				{
+					"screen_name": "LMSNation",
+					"name": "LMS Nation",
+					"id": 309632193,
+					"id_str": "309632193",
+					"indices": [
+						128,
+						138
+					]
+				},
+				{
+					"screen_name": "dadamltps",
+					"name": "David Adam, Ed. D.",
+					"id": 260753316,
+					"id_str": "260753316",
+					"indices": [
+						176,
+						186
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/VuQGx20riO",
+					"expanded_url": "https://twitter.com/NASSP/status/1653026699586547712",
+					"display_url": "twitter.com/NASSP/status/1…",
+					"indices": [
+						232,
+						255
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 7887852,
+			"id_str": "7887852",
+			"name": "Damian N. Bariexca, Ed.D.",
+			"screen_name": "_drdamian",
+			"location": "Perkasie, PA",
+			"description": "Asst. Principal & Instructional Supervisor, @CardinalsLHS ~ #LTPS since 2011 ~ #NJED since 2000 ~ Tweets ≠ opinion of my employers ~ he/him",
+			"url": "https://t.co/GQSCiLbWuN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GQSCiLbWuN",
+							"expanded_url": "http://drdamian.org",
+							"display_url": "drdamian.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2998,
+			"friends_count": 1500,
+			"listed_count": 228,
+			"created_at": "Wed Aug 01 18:19:19 +0000 2007",
+			"favourites_count": 2604,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33645,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320563064266203136/8GZz1FLi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/7887852/1657972223",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1653026699586547700,
+		"quoted_status_id_str": "1653026699586547712",
+		"quoted_status_permalink": {
+			"url": "https://t.co/VuQGx20riO",
+			"expanded": "https://twitter.com/NASSP/status/1653026699586547712",
+			"display": "twitter.com/NASSP/status/1…"
+		},
+		"quoted_status": {
+			"created_at": "Mon May 01 13:20:51 +0000 2023",
+			"id": 1653026699586547700,
+			"id_str": "1653026699586547712",
+			"full_text": "Raise your hand if you are a hard-working, passionate #Principal. Raise your hand if you care about kids and do whatever is needed. Raise your hand if you work long hours, and you're tired. Today, on School Principals' Day, we are sending you love♥️ to end the school year. https://t.co/b4Y7Dipimg",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				273
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Principal",
+						"indices": [
+							54,
+							64
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1653026456149151700,
+						"id_str": "1653026456149151747",
+						"indices": [
+							274,
+							297
+						],
+						"media_url": "http://pbs.twimg.com/media/FvC6zcMX0AM9TWI.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FvC6zcMX0AM9TWI.jpg",
+						"url": "https://t.co/b4Y7Dipimg",
+						"display_url": "pic.twitter.com/b4Y7Dipimg",
+						"expanded_url": "https://twitter.com/NASSP/status/1653026699586547712/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1600,
+								"h": 900,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1653026456149151700,
+						"id_str": "1653026456149151747",
+						"indices": [
+							274,
+							297
+						],
+						"media_url": "http://pbs.twimg.com/media/FvC6zcMX0AM9TWI.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FvC6zcMX0AM9TWI.jpg",
+						"url": "https://t.co/b4Y7Dipimg",
+						"display_url": "pic.twitter.com/b4Y7Dipimg",
+						"expanded_url": "https://twitter.com/NASSP/status/1653026699586547712/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1600,
+								"h": 900,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15894074,
+				"id_str": "15894074",
+				"name": "National Assoc. of Secondary School Principals",
+				"screen_name": "NASSP",
+				"location": "Reston, VA",
+				"description": "We elevate & amplify voices of school leaders. Mission: to transform education through exceptional school leadership via @NASSP, @NatHonorSociety & @NatStuCo.",
+				"url": "https://t.co/4zl7hsjjNu",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/4zl7hsjjNu",
+								"expanded_url": "http://www.nassp.org",
+								"display_url": "nassp.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 47987,
+				"friends_count": 2435,
+				"listed_count": 816,
+				"created_at": "Mon Aug 18 17:33:07 +0000 2008",
+				"favourites_count": 23998,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 21739,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EBEBEB",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1544399263064637441/CRaUJcb4_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1544399263064637441/CRaUJcb4_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15894074/1655381407",
+				"profile_link_color": "D40C0C",
+				"profile_sidebar_border_color": "DFDFDF",
+				"profile_sidebar_fill_color": "F3F3F3",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 39,
+			"favorite_count": 169,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Happy National School Principals' Day to all our #LTPS principals: @MNLockett @MuziLearningLab @JayBilly2 @AmyAmiet @drafischer @LMSNation and the very proud principal of LHS, @dadamltps!  We appreciate all you do for our students! https://t.co/VuQGx20riO"
+	}
+]
